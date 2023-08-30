8cf15d6d60b5a744bea2c9f511ecc07ab71942d9
Lucas Moraes <105878736+moraeszete@users.noreply.github.com>
2023-08-29 22:45:54 -0300

    Contagem de votos
    Implementando a contagem de votos.

--- eleição.cpp
+++ eleição.cpp
@@ -10,11 +10,23 @@ d) 0 = Sai do Sistema
 #include <stdlib.h>
 #include <unistd.h>
 
+struct votos{
+    int voto[4];
+    int numNulo=0, numBranco=0;
+}
+
+int resultado (){
+
+       printf("A quantidade de votos para Vegeta: %d", votos.voto[0]);
+       printf("A quantidade de votos para Babidi: %d", votos.voto[1]);  
+       printf("A quantidade de votos para Piccolo: %d", votos.voto[2]);
+       printf("A quantidade de votos para Gohan: %d", votos.voto[3]);
+}
+
 int main (){
 //numReiniciar setado para 1 para que o loop fique infinito até 
 //chegar no case default, onde o numReiniciar recebe 0 
 //encerrando o loop 
-
     int voto, numReiniciar = 1;
 
  for (int i = 1; i = numReiniciar ;i++){
@@ -31,39 +43,45 @@ int main (){
     scanf("%d", &voto);
 
     switch (voto){
-//linhas 37 e 38, respectivamente, uma seta um tempo para de o comando de clear e reinicar o loop  
+//linhas 37 e 38, respectivamente, uma "seta" um tempo para de o comando de clear e reinicar o loop  
     case 1:
      printf("Seu voto foi salvo!\n" "Vegeta! ");
+     votos.voto[0] ++;
      sleep(5);
      system("cls");
         break;
 
     case 2:
      printf("Seu voto foi salvo!\n" "Babidi! ");
+     votos.voto[1] ++;
      sleep(5);
      system("cls");
         break;
 
     case 3:
      printf("Seu voto foi salvo!\n" "Piccolo! ");
+     votos.voto[2] ++;
      sleep(5);
      system("cls");
         break;
 
     case 4:
      printf("Seu voto foi salvo!\n" "Gohan! ");
+     votos.voto[3] ++;
      sleep(5);
      system("cls");
         break;
     
     case 5:
      printf("Seu voto foi salvo!\n" "NULO! ");
+     votos.numNulo ++;
      sleep(5);
      system("cls");
         break;
     
     case 6:
      printf("Seu voto foi salvo!\n" "Em Branco! ");
+     votos.numBranco ++;
      sleep(5);
      system("cls");
         break;
